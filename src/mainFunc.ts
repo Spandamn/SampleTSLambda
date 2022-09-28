6cce05740b60aa39278fe6be284fceb3d92000ec
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2022-09-28 17:23:35 +0530

    Update mainFunc.ts

--- src/mainFunc.ts
+++ src/mainFunc.ts
@@ -1,11 +1,12 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 
+const addNumbers = function (a: number, b: number): number {
+    return a + b;
+};
+
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-    let a: number, b: number;
-    let eventBody = JSON.parse(event.body);
-    let c = eventBody.a + eventBody.b;
+    let eventBody = JSON.parse(event.body || "");
+    let c = addNumbers(eventBody.a, eventBody.b);
     return {
         statusCode: 200,
         body: JSON.stringify({
